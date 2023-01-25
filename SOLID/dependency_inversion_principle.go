5b3596ff7c8b95f4b5f2c839685861d8e164311a
hiagobalbino <hiagobalbinoo@gmail.com>
2023-01-25 00:07:02 -0300

    improve documentation on constructor examples of Dependency Inversion Principle

--- SOLID/dependency_inversion_principle.go
+++ SOLID/dependency_inversion_principle.go
@@ -1,47 +1,53 @@
-// dependency_inversion_principle is a file to demonstrate what is the idea behind the Dependency Inversion Principle incluing some example.
+// dependency_inversion_principle is a file to demonstrate what is the idea behind the Dependency Inversion Principle including some examples.
 // The file simulates a Bakery.
 package main
 
 import "fmt"
 
-// CookOrder is a struct to represent the model of a cook order to prepare.
+// CookOrder is a struct to represent the model of a cooking order to prepare.
 type CookOrder struct{}
 
-// Prepare is a function that implement the preparation of a cook order.
+// Prepare is a function that implements the preparation of a cooking order.
 func (c CookOrder) Prepare(order string) {
 	fmt.Printf("cooked a %s", order)
 }
 
-// Kitchen is an interface that contains the function that handle with orders.
+// Kitchen is an interface that contains the function that handles orders.
 type Kitchen interface {
-	// Cook is a function that cook a order received through parameter.
+	// Cook is a function that cooks an order received through a parameter.
 	Cook(order string)
 }
 
-// Baker is a struct that represent a baker person and implement the Kitchen interface to handle with orders.
+// Baker is a struct that represents a baker person and implements the Kitchen interface to handle orders.
 type Baker struct {
 	name string
 }
 
-// Cook is a function that cook a order received through parameter.
+// Cook is a function that cooks an order received through a parameter.
 func (b Baker) Cook(order string) {
 	fmt.Printf("cooked a %s", order)
 }
 
+// Bakery is a model to represent a bakery that cooks orders.
 //type Bakery struct {
 //	cookOrder CookOrder
 //}
 
+// NewBakery is a constructor to create a new instance of Bakery to cook orders.
+// This breaks the principle given the Bakery is depending on a concrete struct instead of an abstraction.
 //func NewBakery(cookOrder CookOrder) Bakery {
 //	return Bakery{
 //		cookOrder: cookOrder,
 //	}
 //}
 
+// Bakery is a model to represent a bakery that cooks orders in a kitchen.
 type Bakery struct {
 	kitchen Kitchen
 }
 
+// NewBakery is a constructor to create a new instance of a Bakery to cook orders in a Kitchen.
+// This is a good way and does not break the principle given the Bakery is depending on a Kitchen abstraction instead of a concrete struct.
 func NewBakery(kitchen Kitchen) Bakery {
 	return Bakery{
 		kitchen: kitchen,
@@ -51,9 +57,11 @@ func NewBakery(kitchen Kitchen) Bakery {
 func main() {
 	order := "chocolate cake"
 
+	// Using the constructor that breaks the principle.
 	//	bakery := NewBakery(CookOrder{})
 	//	bakery.cookOrder.Prepare(order)
 
+	// Using the constructor that does not break the principle.
 	baker := Baker{name: "Jhon"}
 	bakery := NewBakery(baker)
 	bakery.kitchen.Cook(order)
