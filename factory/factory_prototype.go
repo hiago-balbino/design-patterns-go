bd408cdad4234c2910c7db0067f5bdb1bc0f6141
hiagobalbino <hiagobalbinoo@gmail.com>
2023-02-07 20:57:38 -0300

    add Factory Prototype code documentation

--- factory/factory_prototype.go
+++ factory/factory_prototype.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Position represents all role position types.
 type Position int
 
 const (
@@ -9,31 +10,39 @@ const (
 	MANAGER
 )
 
+// Employer is an interface to handle employees.
 type Employer interface {
+	// Work is a function that makes the employee works.
 	Work()
 }
 
+// Employee is a base model with basic details of an employee.
 type Employee struct {
 	Name     string
 	Position Position
 }
 
+// Developer is an employee that works as a developer.
 type Developer struct {
 	Employee
 }
 
+// Work is a function that makes the developer works.
 func (d Developer) Work() {
 	fmt.Printf("I'm %s and working as a Developer\n", d.Name)
 }
 
+// Manager is an employee that works as a manager.
 type Manager struct {
 	Employee
 }
 
+// Work is a function that makes the manager works.
 func (m Manager) Work() {
 	fmt.Printf("I'm %s and working as a Manager\n", m.Name)
 }
 
+// NewEmployee is a function that creates a new Employer based on roles.
 func NewEmployee(name string, role Position) Employer {
 	switch role {
 	case DEVELOPER:
